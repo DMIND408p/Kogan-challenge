569ec1ae81cdb94c70f6a7dc554b8360aefc9280
DMIND408P <yyog6@student.monash.edu>
2018-10-16 10:58:26 +1100

    kogan challenge

---
+++ koganChallenge/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  koganChallenge
+//
+//  Created by yogesh anand on 29/3/18.
+//  Copyright © 2018 yogesh anand. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
